f1f6002e002063f59287eec16c1af2c7ce9d46ae
rajavasanthan <rajavasanthan@gmail.com>
2022-02-07 11:48:07 +0530

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { DashboardDirective } from './dashboard.directive';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserCreateComponent } from './user-create/user-create.component';
+import { UserViewComponent } from './user-view/user-view.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    TopBarComponent,
+    DashboardDirective,
+    DashboardComponent,
+    UserListComponent,
+    UserCreateComponent,
+    UserViewComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
